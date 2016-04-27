ff97603c0a9c56d14ffbafe133e9fd67bd19d133
eric kleist <ericjkleist@gmail.com>
2016-04-27 08:10:15 -0700

    fixed style errors

--- Assignment_3.cpp
+++ Assignment_3.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
    // food #1 constants
-   const string FOOD_1_NAME = "Rolled Oats";
+   const  string FOOD_1_NAME = "Rolled Oats";
    const  int FOOD_1_CALORIES_P100G = 375;  // in calories
    const  double FOOD_1_PROTEIN_P100G = 14.29;   // in grams
    const  double FOOD_1_CARBOHYDRATES_P100G = 67.86;   // in grams
@@ -17,7 +17,7 @@ int main()
    const  double FOOD_1_SUGARS_P100G = 0.00;   // in grams
 
    // food #2 constants
-   const string FOOD_2_NAME = "Honey";
+   const  string FOOD_2_NAME = "Honey";
    const  int FOOD_2_CALORIES_P100G = 286;  // in calories
    const  double FOOD_2_PROTEIN_P100G = 0.00;   // in grams
    const  double FOOD_2_CARBOHYDRATES_P100G = 80.95;   // in grams
@@ -25,7 +25,7 @@ int main()
    const  double FOOD_2_SUGARS_P100G = 80.95;   // in grams
 
    // food #3 constants
-   const string FOOD_3_NAME = "Sunflower Seeds";
+   const  string FOOD_3_NAME = "Sunflower Seeds";
    const  int FOOD_3_CALORIES_P100G = 571;  // in calories
    const  double FOOD_3_PROTEIN_P100G = 25.0;   // in grams
    const  double FOOD_3_CARBOHYDRATES_P100G = 21.43;   // in grams
@@ -33,7 +33,7 @@ int main()
    const  double FOOD_3_SUGARS_P100G = 0.00;   // in grams
 
    // food #4 constants
-   const string FOOD_4_NAME = "Pumpkin Seeds";
+   const  string FOOD_4_NAME = "Pumpkin Seeds";
    const  int FOOD_4_CALORIES_P100G = 518;  // in calories
    const  double FOOD_4_PROTEIN_P100G = 32.14;   // in grams
    const  double FOOD_4_CARBOHYDRATES_P100G = 17.86;   // in grams
@@ -41,7 +41,7 @@ int main()
    const  double FOOD_4_SUGARS_P100G = 0.00;   // in grams
 
    // food #5 constants
-   const string FOOD_5_NAME = "Goji Berries";
+   const  string FOOD_5_NAME = "Goji Berries";
    const  int FOOD_5_CALORIES_P100G = 400;  // in calories
    const  double FOOD_5_PROTEIN_P100G = 14.29;   // in grams
    const  double FOOD_5_CARBOHYDRATES_P100G = 67.86;   // in grams
@@ -49,7 +49,7 @@ int main()
    const  double FOOD_5_SUGARS_P100G = 53.57;   // in grams
 
    // food #6 constants
-   const string FOOD_6_NAME = "Chocolate chips";
+   const  string FOOD_6_NAME = "Chocolate chips";
    const  int FOOD_6_CALORIES_P100G = 533;  // in calories
    const  double FOOD_6_PROTEIN_P100G = 6.67;   // in grams
    const  double FOOD_6_CARBOHYDRATES_P100G = 66.67;   // in grams
@@ -57,22 +57,22 @@ int main()
    const  double FOOD_6_SUGARS_P100G = 60.00;   // in grams
 
    // input validation constants
-   const  int INGREDIENT_AMT_LOW = 0;
-   const  int INGREDIENT_AMT_HIGH = 1500;
-   const  int SERVING_SIZE_LOW = 1;
-   const  int SERVING_SIZE_HIGH = 15;
+   const  int MIN_FOOD_WEIGHT = 0;
+   const  int MAX_FOOD_WEIGHT = 1500;
+   const  int MIN_SERVINGS = 1;
+   const  int MAX_SERVINGS = 15;
 
    const  string INDENT = "   ";
 
-   string recipeName, userInputStr, servingSizeStr;
-   int userInputInt, servingSizeInt;
+   string recipeName, userInputStr, servingsStr;
+   int userInputInt, servings;
    double totalCals, totalProtein, totalCarbs, totalFat, totalSugars;
 
    // initialize accumulator variables
-   totalCals =  0;
-   totalProtein  = 0.;
-   totalCarbs  = 0.;
-   totalFat  = 0.;
+   totalCals = 0;
+   totalProtein = 0.;
+   totalCarbs = 0.;
+   totalFat = 0.;
    totalSugars  = 0.;
 
    // print menu
@@ -84,142 +84,134 @@ int main()
    cout << INDENT << "Food #5: " << FOOD_5_NAME << endl;
    cout << INDENT << "Food #6: " << FOOD_6_NAME << endl << endl;
 
-   // name and number of servings for recipe
+   // gets name and number of servings for recipe
    cout << "What are you calling this recipe? ";
    getline(cin, recipeName);
-   cout << "How many servings will this recipe make?";
-   getline(cin, servingSizeStr);
-   istringstream(servingSizeStr) >> servingSizeInt;
-   if (servingSizeInt < SERVING_SIZE_LOW ||
-               servingSizeInt > SERVING_SIZE_HIGH)
-         {
-            cout << "ERROR: Invalid Input. Must be between 1 and 15 servings. "
-                  "Try again.\n\n";
-            return 1;
-         }
+
+   cout << "How many servings will this recipe make? ";
+   getline(cin, userInputStr);
+   istringstream(userInputStr) >> servings;
+   if (servings < MIN_SERVINGS || servings > MAX_SERVINGS)
+   {
+      cout << "\nERROR: Invalid Input. Accepted amount is " << MIN_SERVINGS
+            << " through " << MAX_SERVINGS << " servings. Try again.\n\n";
+      return 1;
+   }
 
    // food #1 ---------------------------------------------------------
-      cout << "How many grams of " << FOOD_1_NAME << "? ";
-      getline(cin, userInputStr);
-      istringstream(userInputStr) >> userInputInt;
-      if (userInputInt < INGREDIENT_AMT_LOW ||
-            userInputInt > INGREDIENT_AMT_HIGH)
-      {
-         cout << "ERROR: Invalid Input. Must be between 0 and 1500 grams. "
-               "Try again.\n\n";
-         return 1;
-      }
-
-
+   cout << "How many grams of " << FOOD_1_NAME << "? ";
+   getline(cin, userInputStr);
+   istringstream(userInputStr) >> userInputInt;
+   if (userInputInt < MIN_FOOD_WEIGHT || userInputInt > MAX_FOOD_WEIGHT)
+   {
+      cout << "\nERROR: Invalid Input. Accepted amount is " << MIN_FOOD_WEIGHT
+            << " through " << MAX_FOOD_WEIGHT << " grams. Try again.\n\n";
+      return 1;
+   }
 
    // update accumulators
    totalCals += userInputInt * (FOOD_1_CALORIES_P100G / 100.);
-   totalProtein  += userInputInt * (FOOD_1_PROTEIN_P100G / 100.);
-   totalCarbs  += userInputInt * (FOOD_1_CARBOHYDRATES_P100G / 100.);
-   totalFat  += userInputInt * (FOOD_1_TOTAL_FAT_P100G / 100.);
-   totalSugars  += userInputInt * (FOOD_1_SUGARS_P100G / 100.);
+   totalProtein += userInputInt * (FOOD_1_PROTEIN_P100G / 100.);
+   totalCarbs += userInputInt * (FOOD_1_CARBOHYDRATES_P100G / 100.);
+   totalFat += userInputInt * (FOOD_1_TOTAL_FAT_P100G / 100.);
+   totalSugars += userInputInt * (FOOD_1_SUGARS_P100G / 100.);
 
    // food #2 ---------------------------------------------------------
    cout << "How many grams of " << FOOD_2_NAME << "? ";
    getline(cin, userInputStr);
    istringstream(userInputStr) >> userInputInt;
-   if (userInputInt < INGREDIENT_AMT_LOW ||
-               userInputInt > INGREDIENT_AMT_HIGH)
-         {
-            cout << "ERROR: Invalid Input. Must be between 0 and 1500 grams. "
-                  "Try again.\n\n";
-            return 1;
-         }
+   if (userInputInt < MIN_FOOD_WEIGHT || userInputInt > MAX_FOOD_WEIGHT)
+   {
+      cout << "\nERROR: Invalid Input. Accepted amount is " << MIN_FOOD_WEIGHT
+            << " through " << MAX_FOOD_WEIGHT << " grams. Try again.\n\n";
+      return 1;
+   }
 
    // update accumulators
    totalCals += userInputInt * (FOOD_2_CALORIES_P100G / 100.);
-   totalProtein  += userInputInt * (FOOD_2_PROTEIN_P100G / 100.);
-   totalCarbs  += userInputInt * (FOOD_2_CARBOHYDRATES_P100G / 100.);
-   totalFat  += userInputInt * (FOOD_2_TOTAL_FAT_P100G / 100.);
-   totalSugars  += userInputInt * (FOOD_2_SUGARS_P100G / 100.);
+   totalProtein += userInputInt * (FOOD_2_PROTEIN_P100G / 100.);
+   totalCarbs += userInputInt * (FOOD_2_CARBOHYDRATES_P100G / 100.);
+   totalFat += userInputInt * (FOOD_2_TOTAL_FAT_P100G / 100.);
+   totalSugars += userInputInt * (FOOD_2_SUGARS_P100G / 100.);
 
    // food #3 ---------------------------------------------------------
    cout << "How many grams of " << FOOD_3_NAME << "? ";
    getline(cin, userInputStr);
    istringstream(userInputStr) >> userInputInt;
-   if (userInputInt < INGREDIENT_AMT_LOW ||
-               userInputInt > INGREDIENT_AMT_HIGH)
-         {
-            cout << "ERROR: Invalid Input. Must be between 0 and 1500 grams. "
-                  "Try again.\n\n";
-            return 1;
-         }
+   if (userInputInt < MIN_FOOD_WEIGHT || userInputInt > MAX_FOOD_WEIGHT)
+   {
+      cout << "\nERROR: Invalid Input. Accepted amount is " << MIN_FOOD_WEIGHT
+            << " through " << MAX_FOOD_WEIGHT << " grams. Try again.\n\n";
+      return 1;
+   }
 
    // update accumulators
    totalCals += userInputInt * (FOOD_3_CALORIES_P100G / 100.);
-   totalProtein  += userInputInt * (FOOD_3_PROTEIN_P100G / 100.);
-   totalCarbs  += userInputInt * (FOOD_3_CARBOHYDRATES_P100G / 100.);
-   totalFat  += userInputInt * (FOOD_3_TOTAL_FAT_P100G / 100.);
-   totalSugars  += userInputInt * (FOOD_3_SUGARS_P100G / 100.);
+   totalProtein += userInputInt * (FOOD_3_PROTEIN_P100G / 100.);
+   totalCarbs += userInputInt * (FOOD_3_CARBOHYDRATES_P100G / 100.);
+   totalFat += userInputInt * (FOOD_3_TOTAL_FAT_P100G / 100.);
+   totalSugars += userInputInt * (FOOD_3_SUGARS_P100G / 100.);
 
    // food #4 ---------------------------------------------------------
    cout << "How many grams of " << FOOD_4_NAME << "? ";
    getline(cin, userInputStr);
    istringstream(userInputStr) >> userInputInt;
-   if (userInputInt < INGREDIENT_AMT_LOW ||
-               userInputInt > INGREDIENT_AMT_HIGH)
-         {
-            cout << "ERROR: Invalid Input. Must be between 0 and 1500 grams. "
-                  "Try again.\n\n";
-            return 1;
-         }
+   if (userInputInt < MIN_FOOD_WEIGHT || userInputInt > MAX_FOOD_WEIGHT)
+   {
+      cout << "\nERROR: Invalid Input. Accepted amount is " << MIN_FOOD_WEIGHT
+            << " through " << MAX_FOOD_WEIGHT << " grams. Try again.\n\n";
+      return 1;
+   }
 
    // update accumulators
    totalCals += userInputInt * (FOOD_4_CALORIES_P100G / 100.);
-   totalProtein  += userInputInt * (FOOD_4_PROTEIN_P100G / 100.);
-   totalCarbs  += userInputInt * (FOOD_4_CARBOHYDRATES_P100G / 100.);
-   totalFat  += userInputInt * (FOOD_4_TOTAL_FAT_P100G / 100.);
-   totalSugars  += userInputInt * (FOOD_4_SUGARS_P100G / 100.);
+   totalProtein += userInputInt * (FOOD_4_PROTEIN_P100G / 100.);
+   totalCarbs += userInputInt * (FOOD_4_CARBOHYDRATES_P100G / 100.);
+   totalFat += userInputInt * (FOOD_4_TOTAL_FAT_P100G / 100.);
+   totalSugars += userInputInt * (FOOD_4_SUGARS_P100G / 100.);
 
    // food #5 ---------------------------------------------------------
    cout << "How many grams of " << FOOD_5_NAME << "? ";
    getline(cin, userInputStr);
    istringstream(userInputStr) >> userInputInt;
-   if (userInputInt < INGREDIENT_AMT_LOW ||
-               userInputInt > INGREDIENT_AMT_HIGH)
-         {
-            cout << "ERROR: Invalid Input. Must be between 0 and 1500 grams. "
-                  "Try again.\n\n";
-            return 1;
-         }
+   if (userInputInt < MIN_FOOD_WEIGHT || userInputInt > MAX_FOOD_WEIGHT)
+   {
+      cout << "\nERROR: Invalid Input. Accepted amount is " << MIN_FOOD_WEIGHT
+            << " through " << MAX_FOOD_WEIGHT << " grams. Try again.\n\n";
+      return 1;
+   }
 
    // update accumulators
    totalCals += userInputInt * (FOOD_5_CALORIES_P100G / 100.);
-   totalProtein  += userInputInt * (FOOD_5_PROTEIN_P100G / 100.);
-   totalCarbs  += userInputInt * (FOOD_5_CARBOHYDRATES_P100G / 100.);
-   totalFat  += userInputInt * (FOOD_5_TOTAL_FAT_P100G / 100.);
-   totalSugars  += userInputInt * (FOOD_5_SUGARS_P100G / 100.);
+   totalProtein += userInputInt * (FOOD_5_PROTEIN_P100G / 100.);
+   totalCarbs += userInputInt * (FOOD_5_CARBOHYDRATES_P100G / 100.);
+   totalFat += userInputInt * (FOOD_5_TOTAL_FAT_P100G / 100.);
+   totalSugars += userInputInt * (FOOD_5_SUGARS_P100G / 100.);
 
    // food #6 ---------------------------------------------------------
    cout << "How many grams of " << FOOD_6_NAME << "? ";
    getline(cin, userInputStr);
    istringstream(userInputStr) >> userInputInt;
-   if (userInputInt < INGREDIENT_AMT_LOW ||
-               userInputInt > INGREDIENT_AMT_HIGH)
-         {
-            cout << "ERROR: Invalid Input. Must be between 0 and 1500 grams. "
-                  "Try again.\n\n";
-            return 1;
-         }
+   if (userInputInt < MIN_FOOD_WEIGHT || userInputInt > MAX_FOOD_WEIGHT)
+   {
+      cout << "\nERROR: Invalid Input. Accepted amount is " << MIN_FOOD_WEIGHT
+            << " through " << MAX_FOOD_WEIGHT << " grams. Try again.\n\n";
+      return 1;
+   }
 
    // update accumulators
    totalCals += userInputInt * (FOOD_6_CALORIES_P100G / 100.);
-   totalProtein  += userInputInt * (FOOD_6_PROTEIN_P100G / 100.);
-   totalCarbs  += userInputInt * (FOOD_6_CARBOHYDRATES_P100G / 100.);
-   totalFat  += userInputInt * (FOOD_6_TOTAL_FAT_P100G / 100.);
-   totalSugars  += userInputInt * (FOOD_6_SUGARS_P100G / 100.);
+   totalProtein += userInputInt * (FOOD_6_PROTEIN_P100G / 100.);
+   totalCarbs += userInputInt * (FOOD_6_CARBOHYDRATES_P100G / 100.);
+   totalFat += userInputInt * (FOOD_6_TOTAL_FAT_P100G / 100.);
+   totalSugars += userInputInt * (FOOD_6_SUGARS_P100G / 100.);
 
-   // convert accumulators to per serving amount
-   totalCals /= servingSizeInt;
-   totalProtein /= servingSizeInt;
-   totalCarbs /= servingSizeInt;
-   totalFat /= servingSizeInt;
-   totalSugars /= servingSizeInt;
+   // update accumulators to per serving amount
+   totalCals /= servings;
+   totalProtein /= servings;
+   totalCarbs /= servings;
+   totalFat /= servings;
+   totalSugars /= servings;
 
    // set decimal precision for output
    cout.setf(ios::fixed);
